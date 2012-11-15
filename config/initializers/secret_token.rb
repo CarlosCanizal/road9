0b93d1929aafaa7addad742b1ba16870926a378d
Carlos Canizal <carlos@codecery.com>
2012-11-15 15:10:04 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoadApp::Application.config.secret_token = 'd5fb666b41ee7e19462fc7e92267f176c63168958b73d3598939c598ba508db350db5fc00dc5f31d1bff10d1e5a556c8c71294099547136e0c75e77dc8a0ce05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RoadApp::Application.config.session_store :cookie_store, key: '_road_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RoadApp::Application.config.session_store :active_record_store
