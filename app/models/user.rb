cbd7d6fddf7b3932bf34bccb4c11f902df7b1e90
Carlos Canizal <carlos@codecery.com>
2012-11-20 10:06:38 -0600

    Ortografia

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :address1, :address2, :email, :home, :lastname,:macAddress, :mobile, :name
   validates :name, :presence=>true
   validates :lastname, :presence=>true
+  validates_numericality_of :mobile, :only_integer => true, :allow_blank => true, :message => "tiene un formato invalido."
+  validates_numericality_of :home, :only_integer => true, :allow_blank => true, :message => "tiene un formato invalido."
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    format:     { with: VALID_EMAIL_REGEX }
 
 end
